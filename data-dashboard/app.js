e6fd5a03f96a51d9a4bb07a5a046be8cdadaca85
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-04-11 22:18:39 -0700

    Added solution for data dashboard.

--- data-dashboard/app.js
+++ data-dashboard/app.js
@@ -1,6 +1,3 @@
-// 1. get data into JS
-// 2. make map
-// 3. make pie chart
 // 4. make bar chart
 // 5. tooltip!
 
@@ -32,8 +29,10 @@ d3.queue()
 
     createMap(width, width * 4 / 5);
     createPie(width, height);
+    createBar(width, height);
     drawMap(geoData, data, currentYear, currentDataType);
     drawPie(data, currentYear);
+    drawBar(data, currentDataType, "");
 
     d3.select("#year")
         .property("min", currentYear)
@@ -43,17 +42,66 @@ d3.queue()
           currentYear = +d3.event.target.value;
           drawMap(geoData, data, currentYear, currentDataType);
           drawPie(data, currentYear);
+          highlightBars(currentYear);
         });
 
     d3.selectAll('input[name="data-type"]')
         .on("change", () => {
+          var active = d3.select(".active").data()[0];
+          var country = active ? active.properties.country : "";
           currentDataType = d3.event.target.value;
           drawMap(geoData, data, currentYear, currentDataType);
+          drawBar(data, currentDataType, country);
         });
+
+    d3.selectAll("svg")
+        .on("mousemove touchmove", updateTooltip);
+
+    function updateTooltip() {
+      var tooltip = d3.select(".tooltip");
+      var tgt = d3.select(d3.event.target);
+      var isCountry = tgt.classed("country");
+      var isBar = tgt.classed("bar");
+      var isArc = tgt.classed("arc");
+      var dataType = d3.select("input:checked")
+                       .property("value");
+      var units = dataType === "emissions" ? "thousand metric tons" : "metric tons per capita";
+      var data;
+      var percentage = "";
+      if (isCountry) data = tgt.data()[0].properties;
+      if (isArc) {
+        data = tgt.data()[0].data;
+        percentage = `<p>Percentage of total: ${getPercentage(tgt.data()[0])}</p>`;
+      }
+      if (isBar) data = tgt.data()[0];
+      tooltip
+          .style("opacity", +(isCountry || isArc || isBar))
+          .style("left", (d3.event.pageX - tooltip.node().offsetWidth / 2) + "px")
+          .style("top", (d3.event.pageY - tooltip.node().offsetHeight - 10) + "px");
+      if (data) {
+        var dataValue = data[dataType] ?
+          data[dataType].toLocaleString() + " " + units :
+          "Data Not Available";
+        tooltip 
+            .html(`
+              <p>Country: ${data.country}</p>
+              <p>${formatDataType(dataType)}: ${dataValue}</p>
+              <p>Year: ${data.year || d3.select("#year").property("value")}</p>
+              ${percentage}
+            `)
+      }
+    }
   });
 
+function formatDataType(key) {
+  return key[0].toUpperCase() + key.slice(1).replace(/[A-Z]/g, c => " " + c);
+}
 
-
+function getPercentage(d) {
+  var angle = d.endAngle - d.startAngle;
+  var fraction = 100 * angle / (Math.PI * 2);
+  return fraction.toFixed(2) + "%";
+}
 
 
 
